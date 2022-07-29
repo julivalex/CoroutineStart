47fbbac2d040c1e1958ecec0ca38754a2afef0e0
yuliaivanenko <y-ivanenko@alfabank.ru>
2022-07-29 21:24:56 +0300

    Корутины "под капотом"

--- app/src/main/java/com/example/coroutinestart/MainActivity.kt
+++ app/src/main/java/com/example/coroutinestart/MainActivity.kt
@@ -2,47 +2,113 @@ package com.example.coroutinestart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
 import android.widget.Toast
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import com.example.coroutinestart.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         binding.buttonLoad.setOnClickListener {
-            loadData()
+//            lifecycleScope.launch {
+//                loadData()
+//            }
+            loadWithoutCoroutine()
         }
     }
 
-    private fun loadData() {
+    private suspend fun loadData() {
+        Log.d("MainActivity", "Load start $this")
         binding.progress.isVisible = true
         binding.buttonLoad.isEnabled = false
         val city = loadCity()
+
         binding.tvLocation.text = city
         val temp = getTemperature(city)
+
         binding.tvTemperature.text = temp.toString()
         binding.progress.isVisible = false
         binding.buttonLoad.isEnabled = true
+        Log.d("MainActivity", "Load finish $this")
     }
 
-    private fun loadCity(): String {
-        Thread.sleep(5000)
+    private suspend fun loadCity(): String {
+        delay(2000)
         return "Moscow"
     }
 
-    private fun getTemperature(city: String): Int {
+    private suspend fun getTemperature(city: String): Int {
         Toast.makeText(
             this,
-            getString(R.string.loading_temperature_toast),
+            getString(R.string.loading_temperature_toast, city),
             Toast.LENGTH_LONG
         ).show()
-        Thread.sleep(5000)
+        delay(2000)
         return 17
+    }
+
+    private fun loadWithoutCoroutine(step: Int = 0, obj: Any? = null) {
+        when (step) {
+            0 -> {
+                binding.progress.isVisible = true
+                binding.buttonLoad.isEnabled = false
+                loadCityWithoutCoroutine {
+                    loadWithoutCoroutine(step = 1, obj = it)
+                }
+            }
+            1 -> {
+                val city = obj as String
+                binding.tvLocation.text = city
+                getTemperatureWithoutCoroutine(city) {
+                    loadWithoutCoroutine(step = 2, obj = it)
+                }
+            }
+            2 -> {
+                val temp = obj as Int
+                binding.tvTemperature.text = temp.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
+        }
+    }
+
+    private fun loadCityWithoutCoroutine(callback: (String) -> Unit) {
+        thread {
+            Thread.sleep(5000)
+            runOnUiThread {
+                callback.invoke("Moscow")
+            }
+        }
+    }
+
+    private fun getTemperatureWithoutCoroutine(city: String, callback: (Int) -> Unit) {
+        thread {
+            runOnUiThread {
+                Toast.makeText(
+                    this,
+                    getString(R.string.loading_temperature_toast, city),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            Thread.sleep(5000)
+            runOnUiThread {
+                callback.invoke(17)
+            }
+        }
     }
 }
