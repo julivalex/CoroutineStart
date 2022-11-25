65a7f80504feb2cca05c362c8e27a9f9041ab427
yuliaivanenko <y-ivanenko@alfabank.ru>
2022-11-25 19:35:11 +0300

    Structured concurrency

--- app/src/main/java/com/example/coroutinestart/MainActivity.kt
+++ app/src/main/java/com/example/coroutinestart/MainActivity.kt
@@ -2,17 +2,10 @@ package com.example.coroutinestart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
-import android.util.Log.d
-import android.widget.Toast
-import androidx.core.view.isVisible
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.coroutinestart.databinding.ActivityMainBinding
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -21,97 +14,14 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.buttonLoad.setOnClickListener {
-            binding.progress.isVisible = true
-            binding.buttonLoad.isEnabled = false
-            val deferredCity: Deferred<String> = lifecycleScope.async {
-                loadCity()
-            }
-            val deferredTemp: Deferred<Int> = lifecycleScope.async {
-                getTemperature()
-            }
-            lifecycleScope.launch {
-                val city = deferredCity.await()
-                binding.tvLocation.text = city
-                val temp = deferredTemp.await()
-                binding.tvTemperature.text = temp.toString()
-                Toast.makeText(
-                    this@MainActivity,
-                    "City: $city Temp: $temp",
-                    Toast.LENGTH_LONG
-                ).show()
-                binding.progress.isVisible = false
-                binding.buttonLoad.isEnabled = true
-            }
-            //loadDataWithoutCoroutine()
-        }
-    }
-
-    private suspend fun loadData() {
-        Log.d("MainActivity2", "Load started: $this")
-        binding.progress.isVisible = true
-        binding.buttonLoad.isEnabled = false
-        val city = loadCity()
-
-        binding.tvLocation.text = city
-        val temp = getTemperature()
-
-        binding.tvTemperature.text = temp.toString()
-        binding.progress.isVisible = false
-        binding.buttonLoad.isEnabled = true
-        Log.d("MainActivity2", "Load finished: $this")
-    }
-
-    private suspend fun loadCity(): String {
-        delay(1000)
-        return "Moscow"
-    }
-
-    private suspend fun getTemperature(): Int {
-        delay(3000)
-        return 17
-    }
-
-    private fun loadDataWithoutCoroutine(step: Int = 0, obj: Any? = null) {
-        when (step) {
-            0 -> {
-                Log.d("MainActivity2", "Load started: $this")
-                binding.progress.isVisible = true
-                binding.buttonLoad.isEnabled = false
-                loadCityWithoutCoroutine { city ->
-                    loadDataWithoutCoroutine(1, city)
-                }
-            }
-            1 -> {
-                val city = obj as String
-                binding.tvLocation.text = city
-                getTemperatureWithoutCoroutine(city) { temp ->
-                    loadDataWithoutCoroutine(2, temp)
-                }
-            }
-            2 -> {
-                val temp = obj as Int
-                binding.tvTemperature.text = temp.toString()
-                binding.progress.isVisible = false
-                binding.buttonLoad.isEnabled = true
-                Log.d("MainActivity2", "Load finished: $this")
-            }
-        }
-    }
-
-    private fun loadCityWithoutCoroutine(callback: (String) -> Unit) {
-        Handler(Looper.getMainLooper()).postDelayed({ callback.invoke("Moscow") }, 2000)
-    }
-
-    private fun getTemperatureWithoutCoroutine(city: String, callback: (Int) -> Unit) {
-        Toast.makeText(
-            this,
-            getString(R.string.loading_temperature_toast, city),
-            Toast.LENGTH_LONG
-        ).show()
-        Handler(Looper.getMainLooper()).postDelayed({ callback.invoke(17) }, 2000)
+        viewModel.method()
+        lifecycleScope
     }
 }
---
+++ app/src/main/java/com/example/coroutinestart/MainViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.coroutinestart
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
+
+class MainViewModel : ViewModel() {
+
+    private val parentJob = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
+
+    fun method() {
+        val childJob1 = coroutineScope.launch {
+            delay(3000)
+            Log.d(LOG_TAG, "first coroutine finished")
+        }
+        val childJob2 = coroutineScope.launch {
+            delay(2000)
+            childJob1.cancel()
+            Log.d(LOG_TAG, "second coroutine finished")
+            Log.d(LOG_TAG, "parent coroutine cancelled: ${parentJob.isCancelled}")
+        }
+        Log.d(LOG_TAG, parentJob.children.contains(childJob1).toString())
+        Log.d(LOG_TAG, parentJob.children.contains(childJob2).toString())
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
+    }
+
+    companion object {
+        private const val LOG_TAG = "MainViewModel"
+    }
+}
